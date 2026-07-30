39bf428768b4a4d3a7f2a54c2bcdd30b1cd94a57
meqabayt <baytbybayt@gmail.com>
2026-07-30 19:47:33 +0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.go
@@ -0,0 +1,33 @@
+func twoSum(nums []int, target int) []int {
+    var neededNums map[int]int = make(map[int]int)
+
+    for leftPointer:=0; leftPointer<=len(nums)/2; leftPointer++ {
+        rightPointer := len(nums) - leftPointer - 1
+
+        leftNum := nums[leftPointer]
+        rightNum := nums[rightPointer]
+
+        if neededNums[target - leftNum] > 0 {
+            return resPrep(neededNums[target-leftNum]-1, leftPointer)
+        } else {
+            neededNums[leftNum] = leftPointer + 1
+        }
+        
+        if neededNums[target - rightNum] > 0 {
+            return resPrep(neededNums[target-rightNum]-1, rightPointer)
+        } else {
+            neededNums[rightNum] = rightPointer + 1
+        }
+    }
+
+    return []int{}
+}
+
+func resPrep(num1, num2 int) []int {
+    fmt.Println(num1, num2)
+    if num1 > num2 {
+        return []int{num2, num1}
+    } else {
+        return []int{num1, num2}
+    }
+}
